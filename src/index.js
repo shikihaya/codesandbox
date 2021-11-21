284165095401654e4274f2a13b7edfde19c922a5
shikihaya <94736168+shikihaya@users.noreply.github.com>
2021-11-22 08:15:58 +0900

    arrow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+/**
+ * アロー関数
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("aaa"));
+
+const func2 = (str) => `${str}です`;
+
+console.log(func2("func2"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 3));
