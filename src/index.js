fb98acd2397e7cee2b0b82db947b0e2ea0d7ec4b
shikihaya <94736168+shikihaya@users.noreply.github.com>
2021-11-22 08:25:44 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -5,17 +5,44 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("aaa"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("aaa"));
 
-const func2 = (str) => `${str}です`;
+// const func2 = (str) => `${str}です`;
 
-console.log(func2("func2"));
+// console.log(func2("func2"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
 
-console.log(func3(10, 3));
+// console.log(func3(10, 3));
+
+/**
+ * 分割代入
+ */
+// object
+// const myProfile = {
+//   name: "aaa",
+//   age: 23
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age, aaa = "不定" } = myProfile;
+
+// const message2 = `名前は${name}です。年齢は${age}です。${aaa}`;
+// console.log(message2);
+
+// array
+// const myProfile = ["aaa", 23];
+
+// const message3 = `名前は${myProfile[0]}で、年齢は${myProfile[1]}です`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+
+// console.log(`${name}と${age}`);
