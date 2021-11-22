07ba3c6a2e64630adaac908e691e8e2841ff874c
shikihaya <94736168+shikihaya@users.noreply.github.com>
2021-11-22 09:59:59 +0900

    mapとかfilterとか

--- src/index.js
+++ src/index.js
@@ -82,3 +82,35 @@
 // console.log(arr8);
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapとかfilter
+ */
+const nameArr = ["田中", "山田", "あああ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name + "aaa";
+// });
+
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "あああ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+
+console.log(newNameArr);
